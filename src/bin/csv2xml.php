1c5b70e46372c1d81abfe9c97f1d8040d3c26141
Gonzalo Villarreal <gonetil@gmail.com>
2018-08-03 09:43:11 -0300

    added support for multiple collections in csv

--- src/bin/csv2xml.php
+++ src/bin/csv2xml.php
@@ -39,8 +39,8 @@ if (!$filename) {
   echo "\r\n              Optional params:
         \r                - authors_group : name used for the author_group_ref (default authors)
         \r                - into_section : section to map imported articles (default IMPORTED)
-        \r                - split_csv [yes | no]: if the input CSV file contains items from many collections, items can be splitted into several (smaller) csv files, one per collection";
-        \r                - limit : maximum number of articles to process (usefull for testing purposes)
+        \r                - split_csv [yes | no]: if the input CSV file contains items from many collections, items can be splitted into several (smaller) csv files, one per collection
+        \r                - limit : maximum number of articles to process (usefull for testing purposes)";
   echo "\r\n      Example:
         \r      php csv2xml.php ~/journal_xyz authors_group=autores into_section=IMPORTADOS split=no\r\n";
   echo "\r      Will process ~/journal_xyz.csv file and output ~/journal_xyz.xml file . Authors will be mapped into autores group (which must exist in OJS), and articles will be placed into IMPORTADOS section (which must also exist in OJS) \r\n \r\n \r\n";
@@ -107,7 +107,7 @@ function splitFileIntoMultipleCSV($filename) {
 
   $files = array();  //array of files to return
   while ( $record = fgetcsv($aggregated_csv) ) {
-      $collection_safe = str_replace( '/','_',$record[1]);
+      $collection_safe = getSingleCollection( str_replace( '/','_',$record[1]) );
       $collection_safe .= '.csv';
       if ($current_collection != $collection_safe)  //new file
       {
@@ -127,4 +127,16 @@ function splitFileIntoMultipleCSV($filename) {
 
   return $files;
 }
+
+
+/** 
+In case an item belogs to more than one collectoin, Dspace exports it as colA || colB || colC . We are only interested in colA.
+This function receives the string with all collections, and returns the first one of the list
+
+**/
+function getSingleCollection($collection_data, $collection_separator="|") {
+	$collections = explode($collection_separator,$collection_data);
+	return $collections[0];
+}
+
 ?>
