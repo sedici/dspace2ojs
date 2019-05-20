04fae3dab302dcc39f7332497b790939e9943fa2
Ezequiel <ezemanzur04@gmail.com>
2019-05-20 11:28:14 -0300

    cambios en template

--- public/js/process_ajax.js
+++ public/js/process_ajax.js
@@ -1,44 +1,77 @@
-console.log("cargue");
 
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+
 
     var buttonProcessFile = $('.process_file');
     var buttonProcessFiles = $('.process_files');
-
-
-    buttonProcessFile.click(function(evt){
-       process_file(this)
+    var buttonArchivedFile = $('.archived_file');
+    var buttonArchivedFiles = $('.archived_files');
+    buttonArchivedFile.click(function (evt) {
+        archived_file(this);
     });
-    buttonProcessFiles.click(function(evt){
+    buttonProcessFile.click(function (evt) {
+        process_file(this);
+    });
+    buttonProcessFiles.click(function (evt) {
         console.log($(this).removeClass('process_files').attr('class'));
-            var class_button=$(this).removeClass('process_files').attr('class');
-            console.log(class_button);  
-            let buttonProcessFile=$(`.parent_${class_button}`);
-                buttonProcessFile.each(function(){
-                    process_file(this)
-                });
+        var class_button = $(this).removeClass('process_files').attr('class');
+        console.log(class_button);
+        let buttonProcessFile = $(`.parent_${class_button}`);
+        buttonProcessFile.each(function () {
+            process_file(this);
+        });
+
+    });
+    buttonArchivedFiles.click(function (evt) {
+        // console.log($(this).removeClass('archived_files').attr('class'));
+        var class_button = $(this).removeClass('archived_files').attr('class');
+        // console.log(class_button);
+        let buttonArchivedFile = $(`.parent_${class_button}`);
+        buttonArchivedFile.each(function () {
+            archived_file(this);
+        });
 
     });
 });
 
 
-function process_file(elem){ 
-    var data= $(elem).attr('value');
-        
-        var id='#'+$(elem).attr('id_td');
+
+function archived_file(elem) {
+    var data = $(elem).attr('value');
+    var id = '#tr_' + $(elem).attr('id_td');
     $.ajax({
-        url: '/home/process_file',
-        data: {"data" : data},
-        success: function(respuesta) {
-            var name= respuesta.response.split("/").pop();
-            $(id).html(`<a href="/${respuesta.response}">${name}</a>`);
-            console.log(respuesta);
+        url: '/home/archived_file',
+        data: { "data": data },
+        success: function (respuesta) {
+            // console.log(respuesta);
+            $(id).remove();
         },
-        error: function(error) {
+        error: function (error) {
             console.log(error);
 
         },
-    });    
-    $(id).html(`<div class="loader"></div>`); 
+    });
+
+}
+function process_file(elem) {
+    var data = $(elem).attr('value');
+
+    var id = '#' + $(elem).attr('id_td');
+    $.ajax({
+        url: '/home/process_file',
+        data: { "data": data },
+        success: function (respuesta) {
+            var name = respuesta.response.split("/").pop();
+            $(id).html(`<a href="/${respuesta.response}">${name}</a>`);
+            // console.log(respuesta);
+        },
+        error: function (error) {
+            console.log(error);
+
+        },
+    });
+    $(id).html(`<div class="loader"></div>`);
+
+
 }
---
+++ public/js/type_file.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+
+});
